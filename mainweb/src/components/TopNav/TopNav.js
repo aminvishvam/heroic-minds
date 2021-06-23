534b91852e81021a895f0caf948db1e122860f0c
Ronak Patel <Ronak.01@outlook.com>
2021-06-23 08:42:01 -0400

    added Navbar buttons

--- mainweb/src/components/TopNav/TopNav.js
+++ mainweb/src/components/TopNav/TopNav.js
@@ -4,12 +4,13 @@ import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Form from 'react-bootstrap/Form';
 import Logo from "../Logo/Logo";
+import "./TopNav.css";
 
 class TopNav extends Component {
 
     render() {
         return (<div className="TopNav">
-            <Navbar expand="lg">
+            <Navbar bg ='white' expand="lg">
                 <Navbar.Brand href="/">
                     <Logo />
                 </Navbar.Brand>
@@ -20,11 +21,17 @@ class TopNav extends Component {
                         style={{ maxHeight: '100px' }}
                         navbarScroll
                     >
-                        <Nav.Link href="/library" className="Nav-text">Library</Nav.Link>
+                        
+                        <Nav.Link href="/about" className="Nav-text ">About</Nav.Link>
+                        <Nav.Link href="/teamAccount" className="Nav-text" >Team Account</Nav.Link>
+                        <Nav.Link href="/book" className="Nav-text">Book</Nav.Link>
+                        
+                        <Nav.Link href="/library" className="Nav-text pl-10" >library</Nav.Link>
                         <Nav.Link href="/community" className="Nav-text" >Community</Nav.Link>
-                        <Nav.Link href="/library" className="Nav-text">Library</Nav.Link>
-                        <Nav.Link href="/community" className="Nav-text" >Community</Nav.Link>
-                    </Nav>
+                        <Nav.Link href="/login" className="Nav-text" >Login</Nav.Link>
+                        <Nav.Link href="/joinNow" className="Nav-text p-0" ><button class='btn btn-dark'>Join Today</button></Nav.Link>
+                        
+                   </Nav>
                     
                 </Navbar.Collapse>
             </Navbar>
