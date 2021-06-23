7e6a2113d972aa4ec06d92062e2d314085107d02
Ronak Patel <Ronak.01@outlook.com>
2021-06-23 10:49:46 -0400

    added Navbar Pages

--- mainweb/src/components/TopNav/TopNav.js
+++ mainweb/src/components/TopNav/TopNav.js
@@ -15,22 +15,33 @@ class TopNav extends Component {
                     <Logo />
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbarScroll" />
-                <Navbar.Collapse id="navbarScroll">
+                <Navbar.Collapse  id="navbarScroll">
                     <Nav
-                        className="mr-auto my-2 my-lg-0"
+                        className="mr-auto my-2 my-lg-0 "
                         style={{ maxHeight: '100px' }}
                         navbarScroll
                     >
-                        
+                        <Nav.Item>
                         <Nav.Link href="/about" className="Nav-text ">About</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
                         <Nav.Link href="/teamAccount" className="Nav-text" >Team Account</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
                         <Nav.Link href="/book" className="Nav-text">Book</Nav.Link>
-                        
-                        <Nav.Link href="/library" className="Nav-text pl-10" >library</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                        <Nav.Link href="/library" className="Nav-text navbar-right" >library</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
                         <Nav.Link href="/community" className="Nav-text" >Community</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
                         <Nav.Link href="/login" className="Nav-text" >Login</Nav.Link>
-                        <Nav.Link href="/joinNow" className="Nav-text p-0" ><button class='btn btn-dark'>Join Today</button></Nav.Link>
-                        
+                        </Nav.Item>
+                        <Nav.Item>
+                        <Nav.Link href="/joinNow" className="Nav-text " ><button class='btn btn-dark'>Join Today</button></Nav.Link>
+                        </Nav.Item>
                    </Nav>
                     
                 </Navbar.Collapse>
