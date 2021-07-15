d32f60e56322bceec02e67c4ffaa0305afcd99c7
saravpatel <sarav8297@gmail.com>
2021-07-16 00:12:52 +0530

    created seaarchbar

--- mainweb/src/components/TopNav/TopNav.js
+++ mainweb/src/components/TopNav/TopNav.js
@@ -6,20 +6,12 @@ import Logo from "../Logo/Logo";
 import { NavLink } from "react-router-dom";
 import "./TopNav.css";
 import { connect } from "react-redux";
-import DropdownArrow from '../DropdownArrow/DropdownArrow';
 
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import ProfilePic from '../../assets/ProfilePic.svg';
+import TopNavLibrary from "../TopNavLibrary/TopNavLibrary";
+import TopNavProfile from "../TopNavProfile/TopNavProfile";
+import SearchBar from "../SearchBar/searchBar";
 
 class TopNav extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      anchorEl: null,
-      profileNode: null,
-    }
-  }
   itemStyle = {
     color: "black",
     fontSize: "15px",
@@ -28,27 +20,8 @@ class TopNav extends Component {
     marginTop: "5px",
   };
 
-  handleClick = (e) => {
-    this.setState({
-      anchorEl: e.currentTarget
-    })
-  }
-
-  handleProfileClick = (e) => {
-    this.setState({
-      profileNode: e.currentTarget
-    })
-  }
-
-  handleClose = () => {
-    this.setState({
-      anchorEl: null
-    })
-  }
-
   renderLinks() {
-    console.log('-------state----', this.state);
-    if (!this.props.authenticated) {
+    if (this.props.authenticated) {
       return (
         <div>
           <Navbar expand="lg">
@@ -62,77 +35,24 @@ class TopNav extends Component {
                 style={{ maxHeight: "100px" }}
                 navbarScroll
               >
-                <div style={{ display: 'flex', flexDirection: 'row', lineHeight: '30px' }} onClick={this.handleClick} >
-                  <label style={{ marginRight: '8px' }}>library</label>
-                  <DropdownArrow />
-                  <Menu
-                    elevation={0}
-                    getContentAnchorEl={null}
-                    anchorEl={this.state.anchorEl}
-                    anchorOrigin={{
-                      vertical: 'bottom',
-                      horizontal: 'center',
+                <TopNavLibrary />
+                <div style={{ display: 'flex', alignItems: 'center', marginRight: '50px' }}>
+                  <NavLink
+                    to="/community"
+                    className="Nav-text"
+                    activeClassName="active"
+                    activeStyle={{
+                      fontWeight: "bold",
+                      color: "#F57C00",
                     }}
-                    transformOrigin={{
-                      vertical: 'top',
-                      horizontal: 'center',
-                    }}
-                    keepMounted
-                    open={!!this.state.anchorEl}
-                    onClose={this.handleClose}
                   >
-                    <MenuItem onClick={this.handleClose}>Keep Listening</MenuItem>
-                    <MenuItem onClick={this.handleClose}>Favourites</MenuItem>
-                    <div style={{ height: '1px', backgroundColor: '#A8A8A8' }}></div>
-                    <MenuItem onClick={this.handleClose}>Recommend a Story/Topic</MenuItem>
-                    <MenuItem style={{ color: '#F57C00' }} onClick={this.handleClose}>Gift an Episode</MenuItem>
-                  </Menu>
-                </div>
-
-                <NavLink
-                  to="/community"
-                  className="Nav-text"
-                  activeClassName="active"
-                  activeStyle={{
-                    fontWeight: "bold",
-                    color: "#F57C00",
-                  }}
-                >
-                  Community
+                    Community
                 </NavLink>
-              </Nav>
-              <Nav.Link style={{ border: 'none' }} className="border-left pl-2 ml-auto">
-                <div style={{ height: '35px', width: '35px', marginRight: '25px', borderRadius: '50px' }} onClick={this.handleProfileClick}>
-
-                  <img height="35px" width="35px" src={ProfilePic} />
-                  <Menu
-                    elevation={0}
-                    getContentAnchorEl={null}
-                    anchorEl={this.state.profileNode}
-                    anchorOrigin={{
-                      vertical: 'bottom',
-                      horizontal: 'center',
-                    }}
-                    transformOrigin={{
-                      vertical: 'top',
-                      horizontal: 'center',
-                    }}
-                    keepMounted
-                    open={!!this.state.profileNode}
-                    onClose={this.handleClose}
-                  >
-                    <label style={{ fontSize: '20px', lineHeight: '24px' }} >User Name</label>
-                    <MenuItem onClick={this.handleClose}>Connect with Friends</MenuItem>
-                    <div style={{ height: '1px', backgroundColor: '#A8A8A8' }}></div>
-                    <MenuItem onClick={this.handleClose}>Profile Settings</MenuItem>
-                    <MenuItem onClick={this.handleClose}>Billing & Payments</MenuItem>
-                    <div style={{ height: '1px', backgroundColor: '#A8A8A8' }}></div>
-                    <MenuItem onClick={this.handleClose}>Get Help & Give Feedback</MenuItem>
-                    <MenuItem onClick={this.handleClose}>Log Out</MenuItem>
-
-                  </Menu>
                 </div>
-              </Nav.Link>
+
+                <SearchBar />
+              </Nav>
+              <TopNavProfile />
             </Navbar.Collapse>
           </Navbar>
         </div>
