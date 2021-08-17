1a797bb2f93097624aac8f81b6a09ce3c8b84699
Ronak Patel <Ronak.01@outlook.com>
2021-08-17 13:42:04 -0400

    Updated Logo

--- mainweb/src/components/Logo/Logo.js
+++ mainweb/src/components/Logo/Logo.js
@@ -2,18 +2,21 @@ import React from "react";
 
 import './Logo.css'
 import LogoIcon from "./LogoIcon/LogoIcon";
+import Nav from "react-bootstrap/Nav";
 import LogoText from "./LogoText/LogoText";
+
+import { Link } from "react-router-dom";
 
 
 const Logo = () => {
     return (
         <div className="Logo">
-        
+            <Nav.Link as={Link} to="/" className="m-0 p-0" eventKey="0">
                 <div className="Logo">
                     <LogoIcon />
                     <LogoText />
                 </div>
-        
+            </Nav.Link>
         </div>
     );
 };
