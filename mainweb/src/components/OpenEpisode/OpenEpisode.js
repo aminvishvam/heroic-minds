63c339c8dfa52f886afa57f30e8b2c4f13679c92
saravpatel <sarav8297@gmail.com>
2021-08-31 12:15:22 +0530

    creating related theme module

--- mainweb/src/components/OpenEpisode/OpenEpisode.js
+++ mainweb/src/components/OpenEpisode/OpenEpisode.js
@@ -3,12 +3,13 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
 
-import { fetchEpisode,fetchEpisodes } from "../../actions/epsiode";
+import { fetchEpisode, fetchEpisodes } from "../../actions/epsiode";
+import { fetchThemes } from "../../actions/theme";
 import Topic from "../Topic/Topic";
 
 class OpenEpisode extends Component {
   state = {};
-  constructor(props){
+  constructor(props) {
     super(props);
     if (!props.episode) {
       const pathName = props.location.pathname;
@@ -19,94 +20,125 @@ class OpenEpisode extends Component {
   componentDidMount() {
     this.props.fetchEpisode(this.props.match.params.id);
     this.props.fetchEpisodes();
+    this.props.fetchThemes();
   }
 
+  renderRelatedTheme() {
+    const topId = this.props.episode.theme1Id;
+    const secondId = this.props.episode.theme2Id;
+    const thirdId = this.props.episode.theme3Id;
+    const relatedThemes = this.props.themes.filter(theme => {
+      return theme._id === topId || theme._id === secondId || theme._id === thirdId
+    })
+    if (topId !== null) {
+      return (<div style={{ width: '100%' }}>
+        <h1>Related Theme</h1>
+        <div className="ui cards">
+          {relatedThemes.filter((theme) => theme.theme !== this.props.episode.title).map((theme) => {
+            return (
+              <div className="card" key={theme._id}>
+                <div className="content negative">
+
+                  <div className="header">
+                    {theme.theme}
+                  </div>
+
+                </div>
+              </div>
+
+            );
+          })}
+        </div>
+      </div>);
+    }
+  }
   renderMoreEpisode() {
-    
     const topId = this.props.episode.theme1Id;
     const topEpi = this.props.episodes.filter(epi => {
-      return epi.theme1Id === topId ||  epi.theme2Id === topId || epi.theme3Id === topId 
+      return epi.theme1Id === topId || epi.theme2Id === topId || epi.theme3Id === topId
     })
     const moreEpi = topEpi.reduce((unique, o) => {
-      if(!unique.some(obj => obj._id === o._id)) {
-        if(!unique.some(obj => obj._id === o._id))
-        unique.push(o);
+      if (!unique.some(obj => obj._id === o._id)) {
+        if (!unique.some(obj => obj._id === o._id))
+          unique.push(o);
       }
       return unique;
-  },[])
-    if(topId !== null){
-      return( <div>
+    }, [])
+    if (topId !== null) {
+      return (<div>
         <h1>Some More Episode</h1>
-        <div style={{display:"flex"}}>
-        {moreEpi.filter((episode)=> episode.title!==this.props.episode.title).map((episode) => {
-        return (
-         
-          <div className="card open-card-image" key={episode._id}>
-            <Link to={`${episode._id}`}>
-            <div className="content negative">
-              <img
-                className="res"
-                alt="/"
-                src={
-                  'https://portfoilo.s3.us-east-2.amazonaws.com/' + episode.imageUrl
-                }
-              />
-              <div className="header">
-                {episode.title}
+        <div style={{ display: "flex" }}>
+          {moreEpi.filter((episode) => episode.title !== this.props.episode.title).map((episode) => {
+            return (
+
+              <div className="card open-card-image" key={episode._id}>
+                <Link to={`${episode._id}`}>
+                  <div className="content negative">
+                    <img
+                      className="res"
+                      alt="/"
+                      src={
+                        'https://portfoilo.s3.us-east-2.amazonaws.com/' + episode.imageUrl
+                      }
+                    />
+                    <div className="header">
+                      {episode.title}
+                    </div>
+                    {episode.topic}
+                  </div>
+                </Link>
               </div>
-              {episode.topic}
-            </div>
-          </Link>
-          </div>
-          
-        );
-      })}
-      </div>
+
+            );
+          })}
+        </div>
       </div>);
     }
   }
 
   render() {
-    if(!this.props.episode){
+    if (!this.props.episode) {
       return null;
     }
-    const { title, topic, description, theme1, theme2, theme3,imageUrl,audioFile} = this.props.episode;
+    const { title, topic, description, theme1, theme2, theme3, imageUrl, audioFile } = this.props.episode;
     return (
       <div className="OpenEpisode">
         <div className="ui container">
-        <h1 className="admin_text_box font-weight-bold">{title}</h1>
-        <img
-              className="res open-card-image"
-              alt="/"
-              src={
-                "https://portfoilo.s3.us-east-2.amazonaws.com/" + imageUrl
-              }
-            />
-                    <p>{topic}</p>
-            <p>{theme1}</p>
-            <p>{theme2}</p>
-            <p>{theme3}</p>
+          <h1 className="admin_text_box font-weight-bold">{title}</h1>
+          <img
+            className="res open-card-image"
+            alt="/"
+            src={
+              "https://portfoilo.s3.us-east-2.amazonaws.com/" + imageUrl
+            }
+          />
+          <p>{topic}</p>
+          <p>{theme1}</p>
+          <p>{theme2}</p>
+          <p>{theme3}</p>
           <div className="justify-content-md-center">
 
             <p>{description}</p>
-    
-         
-         </div>
-         <br/>
-         <br/>
-        
+
+
+          </div>
+          <br />
+          <br />
+
         </div>
         <div className="ui cards">{this.renderMoreEpisode()}</div>
-        <Topic/>
+        <div style={{ width: '100%' }} className="ui cards">{this.renderRelatedTheme()}</div>
+        <Topic />
       </div>
     );
   }
 }
 const mapStateToProps = (state, ownProps) => {
-  return { 
-    episode: state.episode[ownProps.match.params.id], 
-    episodes: Object.values(state.episode)
+  return {
+    episode: state.episode[ownProps.match.params.id],
+    episodes: Object.values(state.episode),
+    themes: Object.values(state.theme)
   };
 };
 
-export default connect(mapStateToProps, { fetchEpisode,fetchEpisodes })(OpenEpisode);
+export default connect(mapStateToProps, { fetchEpisode, fetchEpisodes, fetchThemes })(OpenEpisode);
